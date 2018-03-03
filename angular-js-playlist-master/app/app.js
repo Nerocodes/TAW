1bfd7248a3c264185dd15c2f55b91e8ac0f96394
nerocodes <neropaulej@gmail.com>
2018-03-03 03:25:15 +0100

    first 10 tutorial videos fromnet ninja

---
+++ angular-js-playlist-master/app/app.js
@@ -0,0 +1,30 @@
+var myNinjaApp = angular.module("myNinjaApp", []);
+
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+
+    $scope.message = "hey y'all";
+
+    $scope.ninjas = [
+        {
+            name: "Yoshi",
+            belt: "green",
+            rate: 50
+        },
+        {
+            name: "Crystal",
+            belt: "yellow",
+            rate: 30
+        },
+        {
+            name: "Ryu",
+            belt: "orange",
+            rate: 10
+        },
+        {
+            name: "Shaun",
+            belt: "black",
+            rate: 1000
+        },
+    ];
+
+}]);
